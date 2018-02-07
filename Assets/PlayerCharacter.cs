1e82c46435a56e95f8d223e60f4c029dda55ff32
ddo3 <ddo3@case.edu>
2018-02-07 17:24:53 -0500

    started working on healthbar

--- Assets/PlayerCharacter.cs
+++ Assets/PlayerCharacter.cs
@@ -4,12 +4,28 @@ using System.Collections;
 public class PlayerCharacter : MonoBehaviour {
 	private int _health;
 
+	void OnGUI(){
+		GUI.Label (new Rect (10, 10, 100, 20), HealthBar());
+	}
+
 	void Start() {
 		_health = 5;
+		OnGUI ();
 	}
 
 	public void Hurt(int damage) {
 		_health -= damage;
 		Debug.Log("Health: " + _health);
+		OnGUI ();
+	}
+
+	private string HealthBar(){
+		
+		string healthBar = "";
+		for(int i = 0; i <= _health ; i++){
+			healthBar = healthBar + "*";
+		}
+
+		return healthBar;
 	}
 }
